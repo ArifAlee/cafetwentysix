4cb9ff4d862331d403c83082351688c99eaff537
Arif Ali <Arifalee241@gmail.com>
2025-08-18 00:48:03 +0100

    Update ViewMenuBtn.jsx

--- src/components/ViewMenuBtn.jsx
+++ src/components/ViewMenuBtn.jsx
@@ -1,5 +1,5 @@
-import sandwichMilkshake from "../assets/sandwich-and-milkshake-cropped.jpg";
-import sandwich from "../assets/sandwich.jpg";
+import sandwichMilkshake from "../assets/sandwich-and-milkshake-cropped.JPG";
+import sandwich from "../assets/sandwich.JPG";
 
 export default function ViewMenuBtn() {
   return (
