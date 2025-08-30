59f91b7d87ac32a3c7a1d9de83cde47779609b0b
Arif Ali <Arifalee241@gmail.com>
2025-08-30 12:30:14 +0100

    Update Review.jsx

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,7 +5,7 @@ export default function Review() {
       <a className="flex flex-col justify-center items-center" href="https://g.page/r/CTrLtjMksGFAEBM/review"> 
       <button className="px-2 py-1 rounded-xl border bg-neutral-100 text-sm cursor-pointer"
       >
-        4.9 ⭐⭐⭐⭐✩
+        ⭐⭐⭐⭐⭐
       </button>
         <span className="italic text-sm">Leave a review | get 10% off</span>
       </a>
